1247f1435891c980da3470b402e5ec912f9f5e93
LomininD <saimon1402@mail.ru>
2026-02-12 16:20:52 +0300

    heart program added

--- heart.asm
+++ heart.asm
@@ -2,13 +2,15 @@
 .code
 org 100h
 
-Start: 			mov ax, B800
-				mov es, ax
-				xor bx, bx
-				mov word ptr es:[bx], 4e03h
+Start: 	mov ax, 0B800h
+		mov es, ax
+		xor bx, bx
+		mov word ptr es:[bx + 160 * 2 + 80], 0bc03h
+		; mov byte ptr es:[bx + 1], (4eh or 80h)
 
-				mov ax, 4c00h
-				int 21h 
+		mov ax, 4c00h
+		int 21h 
+
 
 end 			Start
 
